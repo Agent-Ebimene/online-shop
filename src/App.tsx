9d5ed0cccb11ffde1128846f666d7f69da2f0ad1
Agent Ebimene <ebimeneagent@gmail.com>
2022-10-05 16:24:15 +0100

    Create locastic workshop online shop with create-react-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/images/logo.png';
+import cartIcon from './assets/images/eva_shopping-cart-outline.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className='app-header'>
+        <div>
+          <img src={logo}></img>
+        </div>
+        <article className='cart-container'>
+          <img src={cartIcon} alt='cart icon' />
+          <h4>Cart is Empty</h4>
+        </article>
       </header>
     </div>
   );
